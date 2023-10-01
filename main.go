ca94984b09b4eb4d40b00e0754485ad55a53c47a
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-09-30 18:49:08 -0700

    first commits

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "os"
+    "log"
+)
+
+type Day struct {
+    Title string
+    Body []byte
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    // filename := day + ".txt"
+    filename := "day.txt"
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        fmt.Fprintf(w, "Error reading file: %s", filename)
+        return
+    }
+    // return an html file to the client
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", filename, body)
+
+}
 
 func main() {
     fmt.Println("Hello, World!")
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
