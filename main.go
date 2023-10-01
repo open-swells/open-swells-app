9d84be813f5bf3d0d1c9963ec3d4c4ab1fafd9f2
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-09-30 19:10:26 -0700

    gin framework, template added

--- main.go
+++ main.go
@@ -1,32 +1,68 @@
+
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
-    "log"
+	"database/sql"
+	"html/template"
+	"log"
+	"net/http"
+    "bytes"
+//	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+    _ "github.com/mattn/go-sqlite3"
 )
 
-type Day struct {
-    Title string
-    Body []byte
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    // filename := day + ".txt"
-    filename := "day.txt"
-    body, err := os.ReadFile(filename)
-    if err != nil {
-        fmt.Fprintf(w, "Error reading file: %s", filename)
-        return
-    }
-    // return an html file to the client
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", filename, body)
-
+type Prediction struct {
+	WaveHeight float64
+	Direction  float64
+	Period     float64
 }
 
 func main() {
-    fmt.Println("Hello, World!")
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	// Initialize the SQLite3 database connection
+	db, err := sql.Open("sqlite3", "../nbdc-buoydata/db.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Initialize the Gin web framework
+	router := gin.Default()
+
+	// Define a route to handle requests
+	router.GET("/", func(c *gin.Context) {
+		// Query the database to get the latest prediction data
+		var prediction Prediction
+		err := db.QueryRow("SELECT WvHT, MWD, APD FROM predictions ORDER BY datetime DESC LIMIT 1").Scan(&prediction.WaveHeight, &prediction.Direction, &prediction.Period)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		// Parse the HTML template
+		tmpl, err := template.ParseFiles("day.html")
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		// Execute the template with the prediction data
+		htmlBuffer := new(bytes.Buffer)
+		err = tmpl.Execute(htmlBuffer, prediction)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		// Serve the HTML response
+		c.Header("Content-Type", "text/html; charset=utf-8")
+		c.String(http.StatusOK, htmlBuffer.String())
+	})
+
+	// Start the web server
+	router.Run(":8080")
 }
