79492116616f6f2625665e768ca6a965c8dc5fee
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-09-30 22:05:28 -0700

    feat: 3 day forecast with gin and templates

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"database/sql"
 	"html/template"
+    "fmt"
 	"log"
 	"net/http"
     "bytes"
@@ -14,6 +15,7 @@ import (
 )
 
 type Prediction struct {
+    Counter int
 	WaveHeight float64
 	Direction  float64
 	Period     float64
@@ -33,13 +35,20 @@ func main() {
 	// Define a route to handle requests
 	router.GET("/", func(c *gin.Context) {
 		// Query the database to get the latest prediction data
-		var prediction Prediction
-		err := db.QueryRow("SELECT WvHT, MWD, APD FROM predictions ORDER BY datetime DESC LIMIT 1").Scan(&prediction.WaveHeight, &prediction.Direction, &prediction.Period)
-		if err != nil {
-			log.Println(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-			return
-		}
+        var predictions []Prediction
+        for i:= 1; i < 4; i++ {
+            var prediction Prediction
+            query := fmt.Sprintf("SELECT WvHT, MWD, APD FROM predictions WHERE inDays = %d ORDER BY datetime DESC LIMIT 1", i)
+            err := db.QueryRow(query).Scan(&prediction.WaveHeight, &prediction.Direction, &prediction.Period)
+            if err != nil {
+                log.Println(err)
+                c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+                return
+            }
+
+            predictions = append(predictions, Prediction{Counter: i, WaveHeight: prediction.WaveHeight, Direction: prediction.Direction, Period: prediction.Period})
+            // predictions = append(predictions, prediction)
+        }
 
 		// Parse the HTML template
 		tmpl, err := template.ParseFiles("day.html")
@@ -51,7 +60,7 @@ func main() {
 
 		// Execute the template with the prediction data
 		htmlBuffer := new(bytes.Buffer)
-		err = tmpl.Execute(htmlBuffer, prediction)
+		err = tmpl.Execute(htmlBuffer, predictions)
 		if err != nil {
 			log.Println(err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
