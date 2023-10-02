3944f397baa120766d6a82338cbd76bb263c4144
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-10-01 21:15:22 -0700

    big changes to the html template. Formatting and current report

--- main.go
+++ main.go
@@ -2,6 +2,10 @@
 package main
 
 import (
+    //"regexp"
+    "strings"
+    "bufio"
+    "time"
 	"database/sql"
 	"html/template"
     "fmt"
@@ -10,15 +14,82 @@ import (
     "bytes"
 //	"path/filepath"
 
+    // "github.com/gin-contrib/secure"
 	"github.com/gin-gonic/gin"
     _ "github.com/mattn/go-sqlite3"
+    "math"
 )
 
 type Prediction struct {
-    Counter int
+    Title string
 	WaveHeight float64
-	Direction  float64
+	DirectionDegrees  float64
+    DirectionLabel string
 	Period     float64
+}
+
+type CurrentReport struct {
+    Labels []string
+    Values []string
+}
+
+func degreesToCompassLabel(degrees float64) string {
+    // Define the 16-wind compass directions
+    compassLabels := []string{"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"}
+
+    // Normalize degrees to be within the range [0, 360)
+    degrees = math.Mod(degrees, 360.0)
+
+    // Calculate the index in the compassLabels slice based on degrees
+    index := int(math.Round(degrees/22.5)) % len(compassLabels)
+
+    return compassLabels[index]
+}
+
+func getCurrentReport() (CurrentReport, error) {
+    // Make an HTTP GET request to fetch data from the provided link
+    resp, err := http.Get("https://www.ndbc.noaa.gov/data/realtime2/46221.spec")
+    if err != nil {
+        return CurrentReport{}, err
+    }
+    defer resp.Body.Close()
+
+    // Initialize variables to store labels and values
+    var labels []string
+    var values []string
+
+    // Read the response line by line
+    scanner := bufio.NewScanner(resp.Body)
+    rowCounter := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        // Split the first row by spaces to store labels
+        if rowCounter == 0 {
+            labels = strings.Fields(line)
+        } else if rowCounter == 2 {
+            // Skip the second row
+        } else if rowCounter == 3 {
+            // Split the third row by spaces to store values
+            values = strings.Fields(line)
+            break // Stop reading after finding the values
+        }
+
+        rowCounter++
+    }
+
+    if err := scanner.Err(); err != nil {
+        return CurrentReport{}, err
+    }
+    //remove first 5 values of labels and first 4 values of values and replace with current date 
+    labels = append(labels[:0], labels[5:]...)
+    values = append(values[:0], values[4:]...)
+    // add a field "Date" in labels, and add current date in values
+    labels = append(labels, "Date")
+    currentDate := time.Now()
+    values = append(values, currentDate.Format("Mon, Jan 2 15:04"))
+
+    return CurrentReport{Labels: labels, Values: values}, nil
 }
 
 func main() {
@@ -32,22 +103,46 @@ func main() {
 	// Initialize the Gin web framework
 	router := gin.Default()
 
+    router.ForwardedByClientIP = true
+    router.SetTrustedProxies([]string{"127.0.0.1","192.168.1.250", "192.168.1.1"})
+
+
 	// Define a route to handle requests
 	router.GET("/", func(c *gin.Context) {
 		// Query the database to get the latest prediction data
         var predictions []Prediction
+        currentDate := time.Now()
+
         for i:= 1; i < 4; i++ {
+
+            predictionDate := currentDate.AddDate(0, 0, i+1)
+
+            // Format the title for the prediction (e.g., "Mon, Oct 2")
+            title := predictionDate.Format("Mon, Jan 2")
+            //title = "test"
             var prediction Prediction
+            prediction.Title = title
+
             query := fmt.Sprintf("SELECT WvHT, MWD, APD FROM predictions WHERE inDays = %d ORDER BY datetime DESC LIMIT 1", i)
-            err := db.QueryRow(query).Scan(&prediction.WaveHeight, &prediction.Direction, &prediction.Period)
+            err := db.QueryRow(query).Scan(&prediction.WaveHeight, &prediction.DirectionDegrees, &prediction.Period)
             if err != nil {
                 log.Println(err)
                 c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
                 return
             }
+            prediction.DirectionLabel = degreesToCompassLabel(prediction.DirectionDegrees)
 
-            predictions = append(predictions, Prediction{Counter: i, WaveHeight: prediction.WaveHeight, Direction: prediction.Direction, Period: prediction.Period})
-            // predictions = append(predictions, prediction)
+            //fmt.Println(prediction.Title)
+            predictions = append(predictions, prediction)
+        }
+
+        // get the current report
+        currentReport, err := getCurrentReport()
+        fmt.Println(currentReport)
+        if err != nil {
+            log.Println(err)
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+            return
         }
 
 		// Parse the HTML template
@@ -58,9 +153,15 @@ func main() {
 			return
 		}
 
+        // data to send to template
+        data := map[string]interface{}{
+            "Predictions":  predictions,
+            "CurrentReport": currentReport,
+        }
+
 		// Execute the template with the prediction data
 		htmlBuffer := new(bytes.Buffer)
-		err = tmpl.Execute(htmlBuffer, predictions)
+		err = tmpl.Execute(htmlBuffer, data)
 		if err != nil {
 			log.Println(err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
@@ -73,5 +174,6 @@ func main() {
 	})
 
 	// Start the web server
-	router.Run(":8080")
+     router.Run("127.0.0.1:8080")
+	//router.Run(":8080")
 }
